acab8f77d6a0fc81a9a4c09d1abe05e164b7816c
zmnatsakan <pmmnacakan11crew@gmail.com>
2023-12-27 17:09:29 +0300

    Initial Commit

---
+++ Greece/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Greece
+//
+//  Created by mnats on 27.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Greece/GreeceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreeceApp.swift
+//  Greece
+//
+//  Created by mnats on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct GreeceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
